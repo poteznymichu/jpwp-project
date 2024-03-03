4848426dc68b538312043d1ea600e159f33dcc1c
Patr0sss <kkiejno@gmail.com>
2024-03-03 18:48:36 +0100

    new controlls

--- all.py
+++ all.py
@@ -90,11 +90,11 @@ class Game:
                     run = False
 
             keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 player.move("LEFT")
-            if keys[pygame.K_RIGHT]:
+            if keys[pygame.K_RIGHT]or keys[pygame.K_d]:
                 player.move("RIGHT")
-            if keys[pygame.K_UP]:
+            if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
                 player.jump()
 
             player.update_jump(platform)
