cab94c19f90c0e2448f4a97f4cbc0f6dd3823dc0
Patr0sss <kkiejno@gmail.com>
2024-03-12 18:54:02 +0100

    jump sound + code base division into smaller functions

--- PeakyClimber.py
+++ PeakyClimber.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 import sys
 from player import Player
@@ -11,30 +12,41 @@ class Game:
     def drawGameWindow(self, window, backGroundImage):
         window.blit(backGroundImage, (0, 0))
 
-    def main(self):
 
-        clock = pygame.time.Clock()
-        pygame.init()
-        window = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
-        pygame.display.set_caption("Peaky Climber")
+    def handlePlayerScore(self, player, platformGroup, visitedPlatforms) :
 
-        # statyczne platformy
-        platform_group = pygame.sprite.Group()
+        for platform in platformGroup :
+            if player.y == platform.rect.top - player.height :
+                if platform.rect.top not in visitedPlatforms :
+                    player.score += 1
+                    visitedPlatforms.append(platform.rect.top) 
+                    print(f"Player Score : {player.score}")
+    
+
+    def generatePlatforms(self,platformGroup,quantity):
+        
         p_y = MAX_HEIGHT
-        for p in range(5):
-            # p_width = random.randint(150,200)
+        for p in range(quantity):
+
             p_width = 200
-            p_x = random.randint(10, MAX_WIDTH - p_width - 10)
+            p_x = random.randint(30, MAX_WIDTH - p_width - 30)
             p_y -= 130 - p
-            if p == 5 :
-                p_x = -200
 
             platform = Platform(p_x, p_y, "./assets/platform3.png")
-            platform_group.add(platform)
+            platformGroup.add(platform)
 
+    def main(self):
 
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption("Peaky Climber")
+        window = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
         backGroundImage = pygame.image.load("./assets/bg2.png")
-        player = Player(MAX_WIDTH/2, MAX_HEIGHT - PLAYER_HEIGHT , PLAYER_WIDTH, PLAYER_HEIGHT, 8, platform_group)
+      
+        platform_group = pygame.sprite.Group()
+        self.generatePlatforms(platform_group,4)
+
+        player = Player(window.get_width()/2, window.get_height() - PLAYER_HEIGHT , PLAYER_WIDTH, PLAYER_HEIGHT, 8)
 
         start_button_img = pygame.image.load("./assets/start_btn.png")
         button_width, button_height = start_button_img.get_rect().size
@@ -44,14 +56,14 @@ class Game:
         exit_button_width, exit_button_height = exit_button_img.get_rect().size
         exit_button = Button((MAX_WIDTH - exit_button_width) / 2, (MAX_HEIGHT + exit_button_height) / 2, exit_button_img, window)
 
-        font = pygame.font.SysFont(None, 80)
-        title_surface = font.render("Peaky Climber", True, (255, 255, 255))
-        title_rect = title_surface.get_rect()
-        title_rect.center = (window.get_width() // 2, 100)
-
         isPlaying = True
-        isStartMenuOpen = True
+        clock = pygame.time.Clock()
         menuBackgroundImage = pygame.image.load("./assets/menuBackground.png")
+        jumpSound = pygame.mixer.Sound("./assets/jumpSound.mp3")
+
+        Game_State = "menu"
+        visitedPlatforms = []
+
         while isPlaying:
 
             clock.tick(40)
@@ -59,23 +71,26 @@ class Game:
                 if event.type == pygame.QUIT:
                     isPlaying = False
                 if event.type == pygame.KEYDOWN :
-                    if event.key == pygame.K_SPACE and not(player.isJump) :
-                        player.isJump = True
+                    if event.key == pygame.K_SPACE and not(player.isJumping) :
+                        player.isJumping = True
+                        player.jumpSound(jumpSound)
 
-            if isStartMenuOpen :
+            if Game_State == "menu" :
+
                 window.blit(menuBackgroundImage,(0,0))
 
-                # rysowanie buttonow i sprawdzanie akcji jednoczesnie
                 if start_button.draw_button() :
-                    isStartMenuOpen = False
+                    Game_State = "game"
+
                 if exit_button.draw_button() :
                     isPlaying = False
                     
-            else :
-                
+            elif Game_State == "game" :
                 self.drawGameWindow(window, backGroundImage)
-                platform_group.draw(window)
-                player.movement(window.get_width())
+                self.handlePlayerScore(player, platform_group, visitedPlatforms)
+                player.movement(window)
+                player.handlePlatforms(platform_group, window)
+                player.handleGravity(4)
                 player.drawPlayer(window) 
                 
 
--- player.py
+++ player.py
@@ -2,70 +2,78 @@ import pygame
 from consts import *
 class Player:
 
-    def __init__(self, x, y, width, height, vel, platformGroup):
-        self.x = x
+    def __init__(self, x, y, width, height, velocity):
         self.width = width
         self.height = height
-        self.y = MAX_HEIGHT - self.height
-        self.vel = vel
-        self.platformGroup = platformGroup
+        self.x = x
+        self.y = y
+        self.velocity = velocity
         self.player = pygame.image.load("./assets/player.jpg")
         self.rect = self.player.get_rect()
         self.isOnPlatform = False
-        self.isJump = False
+        self.isJumping = False
         self.jumpCount = 8
-        
+        self.score = 0
 
-    def movement(self, windowWidth):
+
+    def movement(self, window):
 
         keys = pygame.key.get_pressed()
-        if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and self.x > self.vel:
-            self.x -= self.vel
+        if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and self.x > self.velocity:
+            self.x -= self.velocity
 
-        if (keys[pygame.K_d] or keys[pygame.K_RIGHT])  and self.x < windowWidth - self.width - self.vel:
-            self.x += self.vel
+        if (keys[pygame.K_d] or keys[pygame.K_RIGHT])  and self.x < window.get_width() - self.width - self.velocity:
+            self.x += self.velocity
 
-
-        if self.isJump  :
+        if self.isJumping  :
             if self.jumpCount >= -8 :
                 self.y -= (self.jumpCount * abs(self.jumpCount)) * 0.8
                 self.jumpCount -= 1
             else:
                 self.jumpCount = 8  
-                self.isJump = False
+                self.isJumping = False
                 
-        gravity = 3.5
-        if not self.isOnPlatform and not self.isJump : 
-            if self.y < MAX_HEIGHT - self.height: 
-                self.y +=gravity ** 2   
-                self.jumpCount = -9
-                
-        for platform in self.platformGroup : 
+
+    def handlePlatforms(self,platform_group, window):
+
+        platform_group.draw(window)
+
+        for platform in platform_group : 
 
             if self.rect.colliderect(platform.rect) :
 
                 if self.rect.centery  <= platform.rect.top and self.jumpCount <= 0 :
+                        
                         self.jumpCount = 8
-
-                        self.isJump = False
-                        if not self.isJump :
+                        self.isJumping = False
+                        if not self.isJumping :
                             self.y = platform.rect.top - self.height               
             else :
                 self.isOnPlatform = False
-                
 
-        for platform in self.platformGroup : 
+        for platform in platform_group : 
             if self.y == platform.rect.top - self.height and self.rect.right >= platform.rect.left and self.rect.left <= platform.rect.right:
                 self.isOnPlatform = True
 
 
+    def handleGravity(self, gravity):
+
+        if not self.isOnPlatform and not self.isJumping : 
+            if self.y < MAX_HEIGHT - self.height: 
+                self.y += gravity ** 2   
+                self.jumpCount = -9
+
         if self.y > MAX_HEIGHT-self.height : 
             self.y = MAX_HEIGHT-self.height
             self.jumpCount = 8
-            self.isJump = False
+            self.isJumping = False
 
 
+    def jumpSound(self,jumpSound) :
+        if self.isOnPlatform or self.y == MAX_HEIGHT - self.height :
+            jumpSound.play() 
   
+
     def drawPlayer(self, window) :
         window.blit(self.player , (self.x , self.y))
         self.rect.update(self.x, self.y, self.width, self.height)
